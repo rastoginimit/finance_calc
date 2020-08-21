bf5c79c726f3659c145e59580d4f1d64d722c9ae
Nimit Rastogi <rastoginimit@gmail.com>
2020-08-21 21:01:39 +1000

    added function for reverse tax calculations

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
-from scripts.calculator import Calculator
+from scripts.calculator import *
 
+'''
+#Calculating Income Tax
 before_tax_income = input("Enter your gross income, before tax: ")
 before_tax_income = float(before_tax_income)
 with open('input/tax_slabs.json') as tax_slabs_file:
@@ -15,3 +17,46 @@ if (before_tax_income > tax_slabs[0]['slab_end']):
 print("Medicare Levy is:", medicare_levy)
 print("Total Tax:", iTax + medicare_levy)
 print("Note: Total Tax above does not include the tax offset you might be eligible for")
+'''
+
+'''
+#Credit card repayments calculations
+calc = Calculator()
+form = Formatter()
+closing_balance = input('Enter the closing balance: ')
+closing_balance = float(closing_balance)
+min_pmt_pct = input("Enter percentage for minimum repayment:")
+min_pmt_pct = float(min_pmt_pct)
+min_amount = input("Enter the minimum amount for the repayment:")
+min_amount = float(min_amount)
+
+cc_interest_rate = input("Enter your credit card's interest rate:")
+cc_interest_rate = float(cc_interest_rate)
+
+min_pmt = calc.calc_cc_min_repayment(closing_balance, min_pmt_pct, min_amount)
+print("Your monthly minimum repayment would be:", min_pmt)
+
+months = calc.calc_months_to_payoff(cc_interest_rate, min_pmt*-1, closing_balance)
+print("Time to pay off the closing balance using minimum repayment:", form.format_months(months))
+
+total_interest = calc.calc_total_interest(cc_interest_rate, closing_balance, min_pmt, months)
+print("Total interest, if only minimum repayments paid:", total_interest)
+
+max_months = 24
+if (months>max_months):
+    pmt=calc.calc_cc_monthly_pmt(cc_interest_rate, 12, closing_balance)
+    pmt = round(pmt, 2)
+    print("To payoff in", max_months, "months, the monthly payment needed is:", pmt)
+    total_interest_new = calc.calc_total_interest(cc_interest_rate, closing_balance, pmt, max_months)
+    print("Total interest if this new amount is paid each month:", total_interest_new)
+    print("Total saving in interest:", round(total_interest - total_interest_new, 2))
+'''
+after_tax_income = input("Enter the income, you want to receive after tax: ")
+after_tax_income = float(after_tax_income)
+with open('input/tax_slabs.json') as tax_slabs_file:
+    tax_slabs = json.load(tax_slabs_file)
+    tax_slabs = tax_slabs["tax_slabs"]
+calc = Calculator()
+pre_tax_income = calc.calc_before_tax_income(after_tax_income, tax_slabs)
+
+print("Your gross income before tax should be:", pre_tax_income)
--- scripts/calculator.py
+++ scripts/calculator.py
@@ -1,4 +1,5 @@
-
+import numpy as np 
+import math 
 class Calculator:
     def __init__(self):
         super().__init__()
@@ -14,4 +15,59 @@ class Calculator:
         tax = prev_slab_tax + (before_tax_income - tax_slabs[-1]["slab_start"])*tax_slabs[-1]["tax_pct"]/100.00
         tax = round(tax,2)
         return tax
-        
+    
+    def generate_rev_tax_slabs(self, tax_slabs):
+        rev_tax_slabs = list()
+        prev_slab_tax = 0.00
+        for slab in tax_slabs:
+            rev_slab = dict()
+            rev_slab["div_factor"] = 100.00 - slab["tax_pct"]
+            rev_slab["sub_factor"] = slab["slab_start"] * slab["tax_pct"]/100.00 - prev_slab_tax
+            rev_slab["slab_start"] = slab["slab_start"] * rev_slab["div_factor"]/100.00 + rev_slab["sub_factor"]
+            rev_slab["slab_end"]   = slab["slab_end"]   * rev_slab["div_factor"]/100.00 + rev_slab["sub_factor"]
+            rev_tax_slabs.append(rev_slab)
+            prev_slab_tax = prev_slab_tax + (slab["slab_end"] - slab["slab_start"])*slab["tax_pct"]/100.00
+        return rev_tax_slabs
+    
+    def calc_before_tax_income(self, after_tax_income, tax_slabs):
+        rev_tax_slabs = self.generate_rev_tax_slabs(tax_slabs)
+        for slab in rev_tax_slabs:
+            if after_tax_income > slab["slab_start"] and after_tax_income <= slab["slab_end"] :
+                before_tax_income=(after_tax_income-slab["sub_factor"])*100.00/slab["div_factor"]
+                before_tax_income=round(before_tax_income,2)
+                return before_tax_income
+        before_tax_income=(after_tax_income-rev_tax_slabs[-1]["sub_factor"])*100.00/rev_tax_slabs[-1]["div_factor"]
+        before_tax_income=round(before_tax_income,2)
+        return before_tax_income
+
+    def calc_cc_min_repayment(self, closing_balance, pct, min_amount):
+        return max([min_amount, math.ceil(closing_balance*pct/100)])
+    
+    def calc_months_to_payoff(self, rate, pmt, closing_balance):
+        nper = np.nper(rate/12/100.00, pmt, closing_balance)
+        return nper
+    def calc_cc_monthly_pmt(self, rate, months, closing_balance):
+        pmt = -np.pmt(rate/12/100.00, 24, closing_balance)
+        return pmt
+    def calc_total_interest(self, rate, closing_balance, pmt, nper):
+        per = np.arange(nper) + 1
+        ipmt = -np.ipmt(rate/12/100.00, per, nper, closing_balance)
+        totalinterest = np.sum(ipmt)
+        totalinterest = round(totalinterest, 2)
+        return totalinterest
+
+class Formatter:
+    def format_months(self, months):
+        months = math.ceil(months)
+        years=math.floor(months/12)
+        remaining_months = months - (years*12)
+        if (years == 0):
+            formatted_years = ""
+            formatted_months = str(remaining_months) + " Months"
+        else:
+            formatted_years = str(years) + " Years"
+            if (remaining_months == 0):
+                formatted_months = ""
+            else:
+                formatted_months = " and " + str(remaining_months) + " Months"
+        return formatted_years + formatted_months
